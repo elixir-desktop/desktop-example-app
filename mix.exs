2f0ce926cc5c258fbc0c60b3c0d557d3023fe1e7
Dominic Letz <dominic@diode.io>
2021-10-27 19:04:31 +0200

    Updated desktop version, using github to fix dependency issue

--- mix.exs
+++ mix.exs
@@ -57,8 +57,8 @@ defmodule Todo.MixProject do
   defp deps do
     [
       {:ecto_sqlite3, "~> 0.7"},
-      {:desktop, "~> 1.3"},
       # {:desktop, path: "../desktop"},
+      {:desktop, github: "elixir-desktop/desktop", tag: "v1.3.1"},
 
       # Phoenix
       {:phoenix, "~> 1.6"},
