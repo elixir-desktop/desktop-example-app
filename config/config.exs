f7eb26e87506ae8718e4db32e4346d4bf115d576
Adam Roses Wight <spam@ludd.net>
2022-08-16 23:48:28 +0200

    Migrate from Mix.Config to core Config (#11)
    For more background, see
    https://hexdocs.pm/elixir/1.12/Config.html#module-migrating-from-use-mix-config

--- config/config.exs
+++ config/config.exs
@@ -44,4 +44,4 @@ config :todo_app,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
