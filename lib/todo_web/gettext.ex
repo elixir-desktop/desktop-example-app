a591f54f7d479d82150560a9bb652ef7f6c76c03
Dominic Letz <dominic.letz@berlin.de>
2025-02-27 22:22:41 -0700

    Update versions & CI (#27)
    * Updated wxWidgets repo and deps
    
    * Update base versions
    
    * fixup
    
    * fix_macos
    
    * fixup
    
    * try wxWidgets v3.2.6
    
    * fixup linux
    
    * Fix iconset creation
    
    * try to fix windows build

--- lib/todo_web/gettext.ex
+++ lib/todo_web/gettext.ex
@@ -5,7 +5,7 @@ defmodule TodoWeb.Gettext do
   By using [Gettext](https://hexdocs.pm/gettext),
   your module gains a set of macros for translations, for example:
 
-      import TodoWeb.Gettext
+      use Gettext, backend: TodoWeb.Gettext
 
       # Simple translation
       gettext("Here is the string to translate")
@@ -20,5 +20,5 @@ defmodule TodoWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :todo_app
+  use Gettext.Backend, otp_app: :todo_app
 end
